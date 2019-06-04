130388a846c033ea85ee8ded13e29394b69fde59
Andreas Weber <weber@alluris.de>
2019-06-04 09:20:35 +0200

    Brainstorming erweitert, systemd-analyze an echtem Beispiel

--- systemd/get_temp.sh
+++ systemd/get_temp.sh
@@ -1,16 +1,14 @@
 #!/bin/bash -e
 
-while $(sleep 3);
+while true;
 do
 
   # Außentemperatur lesen
   temp=$(curl -s https://demo.volkszaehler.org/middleware.php/data/12345678-1234-1234-1234-123456789012.json?from=now | jq '.data.average')
-  str=$(LANG=C printf "Außentemperatur = %.1f°C\n" "$temp")
-  echo $str
 
-  #if (( $(echo "$temp > 15.0" |bc -l) ));
-  #then
-  #  notify-send "$str. Bitte Fenster im Wintergarten klappen"
-  #fi
+  # und formatiert ausgeben
+  LANG=C printf "Außentemperatur = %.1f°C\n" "$temp"
 
+  # 10s warten
+  sleep 10
 done
